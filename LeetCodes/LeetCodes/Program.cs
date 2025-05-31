43dcb6f6f6ef0ccb10813a9b681c7a5e1041b016
ftomelin <tomelinfelipe@Live.com>
2025-05-31 16:56:57 -0300

    TwoSum

---
+++ LeetCodes/LeetCodes/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCodes;
+
+var twoSum = new TwoSum();
+var response = twoSum.TwoSumMethodBestCase([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 1);
+
+System.Console.WriteLine(response);
---
+++ LeetCodes/LeetCodes/TwoSum.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodes;
+
+public class TwoSum
+{
+    // O(N^2)
+    public int[] TwoSumMethodBruteForce(int[] nums, int target)
+    {
+        for (var i = 0; i <= nums.Length - 1; i++)
+        {
+            for (var j = 0; j <= nums.Length - 1; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                if (nums[i] + nums[j] == target)
+                {
+                    var valorN1 = i;
+                    var valorN2 = j;
+                    return [valorN1, valorN2];
+                }
+            }
+        }
+
+        return [0, 0];
+    }
+    
+    // O(N)
+    public int[] TwoSumMethodBestCase(int[] nums, int target)
+    {
+        var hasher = new Dictionary<int, int>();
+        for (var i = 0; i <= nums.Length - 1; i++)
+        {
+            var number = hasher.ContainsKey(nums[i]) ;
+            if (number)
+            {
+                return [hasher[nums[i]], i];
+            }
+
+            if (hasher.ContainsKey(target - nums[i]))
+            {
+                continue;
+            }
+            
+            hasher.Add(target - nums[i], i);
+        }
+
+        return [];
+    }
+}
